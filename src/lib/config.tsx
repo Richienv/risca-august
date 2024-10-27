c261d1f3ac6d59f18750a7a53a35ba32335b93ba
Richie Kid Novell <94738041+Richienv@users.noreply.github.com>
2024-10-27 14:08:33 +0800

    Update config.tsx

--- src/lib/config.tsx
+++ src/lib/config.tsx
@@ -7,8 +7,8 @@ import { RiInstagramFill } from "react-icons/ri";
 export const BLUR_FADE_DELAY = 0.15;
 
 export const siteConfig = {
-  name: "Alife by Vision",
-  description: "Alife by Vision adalah komunitas bisnis asuransi.",
+  name:  "Copyright © 2024 All rights reserved",",
+  description: "",
   url: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
   keywords: ["SaaS", "Template", "Next.js", "React", "Tailwind CSS"],
   links: {
