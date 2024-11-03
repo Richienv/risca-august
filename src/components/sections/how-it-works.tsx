0681a1c44a435e26f9ad0f4813790ce501cebb5d
Richie Kid Novell <94738041+Richienv@users.noreply.github.com>
2024-11-03 18:24:49 +0800

    Update how-it-works.tsx

--- src/components/sections/how-it-works.tsx
+++ src/components/sections/how-it-works.tsx
@@ -9,24 +9,24 @@ import Ripple from "@/components/ui/ripple";
 const data = [
   {
     id: 1,
-    title: "1. Isi Formulir",
-    content: "Cukup isi formulir singkat dengan informasi dasar Anda. Proses ini hanya memakan waktu beberapa menit.",
-    image: "/dashboard.png",
-    icon: <ClipboardEdit className="w-6 h-6 text-primary" />,
+    title: "1. Isi Formulir Singkat",
+    content: "Fill it with your basic information. This process only takes a few minutes.",
+    image: "/join.png",
+    icon: <ClipboardEdit className="w-8 h-8 sm:w-6 sm:h-6 text-primary" />,
   },
   {
     id: 2,
-    title: "2. Kami Akan Menghubungi Anda",
-    content: "Tim kami akan menghubungi Anda dalam waktu kurang dari 15 menit untuk membahas lebih lanjut tentang peluang dan manfaat bergabung dengan Alife.",
-    image: "/dashboard.png",
-    icon: <PhoneCall className="w-6 h-6 text-primary" />,
+    title: "2. We Will Contact You Personally",
+    content: "Me and my team will read your information and contact you within 15 minutes to discuss with you.",
+    image: "/contact.png",
+    icon: <PhoneCall className="w-8 h-8 sm:w-6 sm:h-6 text-primary" />,
   },
   {
     id: 3,
-    title: "3. Undangan Event Pertama",
-    content: "Setelah berbicara dengan tim kami, Anda akan langsung diundang ke event pertama Alife, di mana Anda bisa mulai networking dan belajar.",
-    image: "/dashboard.png",
-    icon: <Calendar className="w-6 h-6 text-primary" />,
+    title: "3. Your First Event Invitation",
+    content: "After talking with my team, you will be invited to our first event, where you can start networking and learning.",
+    image: "/first.png",
+    icon: <Calendar className="w-8 h-8 sm:w-6 sm:h-6 text-primary" />,
   },
 ];
 
@@ -34,7 +34,7 @@ export default function HowItWorks() {
   return (
     <>
       <BlueGradientSeparator />
-      <Section className="py-1 md:py-1 relative overflow-hidden">
+      <Section className="py-1 relative overflow-hidden">
         <Ripple 
           className="absolute right-[35%] md:right-[0%] top-[-100%] md:bottom-[-3%] transform translate-x-1/4 translate-y-1/4" 
           mainCircleSize={100}
@@ -42,15 +42,16 @@ export default function HowItWorks() {
         />
         
         <div className="relative z-10">
-          <div className="text-center mb-6 md:mb-8">
-            <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold mb-3 md:mb-4">Cara Bergabung</h2>
-            <hr className="w-20 md:w-24 mx-auto border-t-2 border-primary mb-3 md:mb-4" />
+          <div className="text-center mb-2">
+            <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold mb-1">Cara Bergabung</h2>
+            
+            <hr className="w-20 md:w-24 mx-auto border-t-2 border-primary mb-1" />
           </div>
           <div className="lg:overflow-x-visible">
             <Features 
               data={data} 
-              customTitleClassName="text-left text-base md:text-lg lg:text-xl font-semibold mb-1 bg-gradient-to-r from-blue-500 via-purple-500 to-red-500 text-transparent bg-clip-text"
-              customContentClassName="text-sm md:text-base text-muted-foreground"
+              customTitleClassName="text-left text-xl sm:text-lg md:text-xl lg:text-2xl font-bold mb-2 bg-gradient-to-r from-blue-500 via-purple-500 to-red-500 text-transparent bg-clip-text"
+              customContentClassName="text-lg sm:text-base md:text-lg text-muted-foreground"
             />
           </div>
         </div>
