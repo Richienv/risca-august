21b9e777e51a8375286f490e60a8ae937e63cc74
Richie Kid Novell <94738041+Richienv@users.noreply.github.com>
2024-11-04 16:51:08 +0800

    Update config.tsx

--- src/lib/config.tsx
+++ src/lib/config.tsx
@@ -6,12 +6,17 @@ import { RiInstagramFill } from "react-icons/ri";
 export const BLUR_FADE_DELAY = 0.15;
 
 export const siteConfig = {
-  name: "Copyright © 2024 All rights reserved",
-  description: "",
+  name: "Martha Teja Insurance Community",
+  description: "Join our thriving insurance business community. Learn, grow, and succeed together with Martha's mentorship and support network.",
   url: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
-  keywords: ["SaaS", "Template", "Next.js", "React", "Tailwind CSS"],
+  keywords: ['insurance business',
+    'Martha insurance',
+    'business community',
+    'insurance mentorship',
+    'financial freedom',
+    'business opportunity'],
   links: {
-    email: "alife@gmail.com",
+    email: "ask.marthateja@gmail.com",
     twitter: "https://twitter.com/magicuidesign",
     discord: "https://discord.gg/87p2vpsat5",
     github: "https://github.com/magicuidesign/magicui",
