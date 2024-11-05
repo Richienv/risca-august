8b97a0a05da0610324cabb8db5e15b4ac78efd02
Richie Kid Novell <94738041+Richienv@users.noreply.github.com>
2024-11-05 20:30:03 +0800

    Update hero.tsx

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -24,29 +24,12 @@ function HeroPill() {
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8, ease }}
     >
-      <Link href="/blog/introducing-alife-by-vision" className="flex items-center">
-        <p
-          style={{ "--shimmer-width": "100px" } as React.CSSProperties}
-          className="mx-auto max-w-md text-neutral-600/80 dark:text-neutral-400/80 animate-shimmer bg-clip-text bg-[length:var(--shimmer-width)_100%] bg-no-repeat [background-position:0_0] [transition:background-position_1s_cubic-bezier(.6,.6,0,1)_infinite] bg-gradient-to-r from-neutral-100 via-neutral-900/80 via-50% to-neutral-100 dark:from-neutral-900 dark:via-neutral-100/80 dark:to-neutral-900 inline-flex items-center justify-center"
-        >
-          <span>Bisnis Yang Terbukti Mencetak Income Milyaran</span>
-        </p>
-        <svg
-          width="15"
-          height="15"
-          viewBox="0 0 15 15"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5"
-        >
-          <path
-            d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
-            fill="currentColor"
-            fillRule="evenodd"
-            clipRule="evenodd"
-          />
-        </svg>
-      </Link>
+      <p
+        style={{ "--shimmer-width": "100px" } as React.CSSProperties}
+        className="mx-auto max-w-md text-neutral-600/80 dark:text-neutral-400/80 animate-shimmer bg-clip-text bg-[length:var(--shimmer-width)_100%] bg-no-repeat [background-position:0_0] [transition:background-position_1s_cubic-bezier(.6,.6,0,1)_infinite] bg-gradient-to-r from-neutral-100 via-neutral-900/80 via-50% to-neutral-100 dark:from-neutral-900 dark:via-neutral-100/80 dark:to-neutral-900 inline-flex items-center justify-center"
+      >
+        <span>Bisnis Yang Terbukti Mencetak Income Milyaran</span>
+      </p>
     </motion.div>
   );
 }
@@ -64,7 +47,7 @@ function HeroTitles() {
           delay: 0.2,
         }}
       >
-        Kumpulan Anak-Anak Muda  High Achievers Income Milyaran
+        WAKTUNYA GEN Z MELESAT!
       </motion.h1>
       <motion.p
         className="mb-8 text-sm tracking-tight text-muted-foreground md:text-base text-balance text-center"
@@ -76,7 +59,7 @@ function HeroTitles() {
           ease: [0.16, 1, 0.3, 1],
         }}
       >
-        <span className="text-white">Kosongkan Gelas Kamu dan Belajar Bareng Kami <span className="font-bold underline">dan Boost Income Kamu Jadi 3 Digit</span> dengan sistem yang kami punya.</span>
+        <span className="text-white">Ngga perlu nunggu tua buat sukses! Disini kamu bisa mulai perjalanan menuju <span className="font-bold underline">Income Ratusan Juta - Miliaran Per Bulan</span></span>
       </motion.p>
     </div>
   );
