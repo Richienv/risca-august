facf552b891d2c4dbb647bcd34bf4fc56c8c45b2
Richie Kid Novell <94738041+Richienv@users.noreply.github.com>
2024-11-10 19:09:20 +0800

    Update solution.tsx

--- src/components/sections/solution.tsx
+++ src/components/sections/solution.tsx
@@ -9,7 +9,7 @@ import { useRef } from 'react';
 const transformations = [
   {
     title: "Kita Ajarkan Semua Skills & Techniques For Success!",
-    description: "Kamu akan mendapatkan akses eksklusif ke kelas training kita, yang terdiri dari lebih dari 1000+ anggota dan saya siap membantu kamu dalam setiap langkah bisnis!",
+    description: "Kamu akan mendapatkan akses eksklusif ke kelas training kita, yang terdiri dari lebih dari 1000+ anggota dan aku siap membantu kamu dalam setiap langkah bisnis!",
     image: "/belajar.png",
   },
   {
